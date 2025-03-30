e0d6854d7eb75cabdb41fd181089189f3e68d99c
Evan You <yyx990803@gmail.com>
2025-03-30 13:16:39 +0800

    +axios retry

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import {toast} from "react-toastify";
+import axiosRetry from "axios-retry";
 
 const params = new URLSearchParams(window.location.search);
 export const apiAddress = params.get("api") ?? `${window.location.origin}/`
@@ -7,6 +8,13 @@ export const apiAddress = params.get("api") ?? `${window.location.origin}/`
 export const client = axios.create({
     baseURL: apiAddress
 })
+
+axiosRetry(client, {
+    retries: 3,
+    retryDelay: (retryCount) => {
+        return retryCount * 100;
+    }
+});
 
 client.interceptors.response.use((config) => {
     return config
