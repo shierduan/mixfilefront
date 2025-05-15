901e11e649ba4e8ecda6e3fad92d5d0f45b83f01
1 <386029724@qq.com>
2025-05-15 16:23:50 +0800

    Update UploadDialog.jsx

--- src/components/file/upload/UploadDialog.jsx
+++ src/components/file/upload/UploadDialog.jsx
@@ -5,6 +5,7 @@ import {notifyMsg} from "../../../utils/CommonUtils.js";
 import {addDialog, dialogProxy} from "../../../utils/DialogContainer.jsx";
 import {proxy, ref, useSnapshot} from "valtio";
 import FileExportDialog from "../FileExport.jsx";
+import {useEffect} from "react";
 
 const Container = styled.div`
     display: flex;
@@ -90,6 +91,21 @@ function UploadDialog() {
             )
         }
     }
+
+    useEffect(() => {
+        const listener = function (event) {
+            if (!complete) {
+                event.preventDefault()
+                const msg = '文件正在上传中,是否确认关闭?'
+                event.returnValue = msg
+                return msg
+            }
+        }
+        window.addEventListener('beforeunload', listener)
+        return () => {
+            window.removeEventListener('beforeunload', listener)
+        }
+    }, [complete]);
 
     return (
         <Container className={'shadow'}>
